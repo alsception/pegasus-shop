742533912233b8adac95a0b9cbd3823322c45083
nix <juric.biomedica@gmail.com>
2025-07-08 17:36:19 +0200

    Added theme controller

--- frontend/src/core/navigation/Header.svelte
+++ frontend/src/core/navigation/Header.svelte
@@ -5,21 +5,31 @@
 <div class="navbar shadow-sm fixed bg-slate-950 z-50">
   <div class="navbar-start">
     <div class="dropdown bg-neutral-800">
-      <div tabindex="0" role="button" class="btn btn-ghost btn-circle bg-neutral-800 hover:bg-neutral-700">
+      <div
+        tabindex="0"
+        role="button"
+        class="btn btn-ghost btn-circle bg-neutral-800 hover:bg-neutral-700"
+      >
         <i class="fas fa-bars text-xl"></i>
       </div>
 
-      <PrimaryMenu/>
-
+      <PrimaryMenu />
     </div>
   </div>
   <div class="navbar-center">
     <a href="/#/home">
-      <h1 class="pp text-lg sm:text-xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-sky-400 via-blue-500 to-violet-600 text-transparent bg-clip-text font-mono uppercase tracking-widest drop-shadow-md yellowtail-regular "
-      >Pegasus</h1>
+      <h1
+        class="pp text-lg sm:text-xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-sky-400 via-blue-500 to-violet-600 text-transparent bg-clip-text font-mono uppercase tracking-widest drop-shadow-md yellowtail-regular"
+      >
+        Pegasus
+      </h1>
     </a>
   </div>
   <div class="navbar-end">
-    <!-- OVDE CE DA IDE: THEME, SETTINGS?, I LOGOUT -->
+    <label class="swap swap-rotate mr-4" title="Toggle Dark Mode">
+      <input type="checkbox" class="theme-controller" value="dark" />
+      <span class="swap-off text-xl">☀️</span>
+      <span class="swap-on text-xl">🌙</span>
+    </label>
   </div>
 </div>
